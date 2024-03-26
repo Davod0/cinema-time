6d3a6b476ba549c25270c48ac0fe5a95ef3c7cac
Davo <davodnikoyi.gmail.com>
2024-03-26 15:29:41 +0100

    implemented the Cinema.MinimalWebApi

--- Cinema.MinimalWebApi/Program.cs
+++ Cinema.MinimalWebApi/Program.cs
@@ -3,10 +3,11 @@ using Cinema.Infrastructure;
 using Cinema.Core;
 using Microsoft.EntityFrameworkCore;
 using Swashbuckle;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
@@ -16,9 +17,32 @@ internal class Program
         builder.Services.AddSwaggerGen();
         var app = builder.Build();
 
-        app.MapGet("/movie", () => "Hello World!");
+        app.MapGet("/", () => "Hello World!");
+
+        app.MapGet("/minimalApi/movie", async (MovieService _ms) => await _ms.GetAllMoviesAsync());
+
+        app.MapGet("/minimalApi/postMovie", async (MovieService _ms, Movie m) =>
+        {
+            if (await _ms.AddMovieAsync(m) != null)
+            {
+                return Results.Created("/minimalApi/postMovie", m);
+            }
+            return Results.BadRequest();
+        });
+
+        app.MapGet("/minimalApi/deleteMovie", async (MovieService _ms, Movie m) =>
+        {
+            if (await _ms.DeleteMovieAsync(m) != null)
+            {
+                return Results.Ok(m);
+            }
+            return Results.BadRequest(); ;
+
+        });
 
         app.Run();
     }
 }
+
+
 
