653c31e6786322f66cc29ec3508aab544643f532
Davo <davodnikoyi.gmail.com>
2024-03-26 16:54:51 +0100

    Chenags in Cinema.MinimalWebApi program.cs and Cinema.Infrastruture DbContextInfrastruture.cs

--- Cinema.MinimalWebApi/Program.cs
+++ Cinema.MinimalWebApi/Program.cs
@@ -17,28 +17,29 @@ internal class Program
         builder.Services.AddSwaggerGen();
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
 
-        app.MapGet("/minimalApi/movie", async (MovieService _ms) => await _ms.GetAllMoviesAsync());
+        app.MapGet("/minimalApi/movie", async (IMovieRepository _repo) => await _repo.GetAllMoviesAsync());
 
-        app.MapGet("/minimalApi/postMovie", async (MovieService _ms, Movie m) =>
+        app.MapPost("/minimalApi/postMovie", async (IMovieRepository _repo, Movie m) =>
         {
-            if (await _ms.AddMovieAsync(m) != null)
+            if (await _repo.AddMovieAsync(m) != null)
             {
                 return Results.Created("/minimalApi/postMovie", m);
             }
             return Results.BadRequest();
         });
 
-        app.MapGet("/minimalApi/deleteMovie", async (MovieService _ms, Movie m) =>
-        {
-            if (await _ms.DeleteMovieAsync(m) != null)
-            {
-                return Results.Ok(m);
-            }
-            return Results.BadRequest(); ;
+        // app.MapDelete("/minimalApi/deleteMovie", async (IMovieRepository _repo, Movie m) =>  // FIXA
+        // {
+        //     if (await _repo.DeleteMovieAsync(m) != null)
+        //     {
+        //         return Results.Ok();
+        //     }
+        //     return Results.BadRequest(); ;
 
-        });
+        // });
+
+        app.MapGet("/", async () => await Task.Run(() => "Hello World!"));
 
         app.Run();
     }
