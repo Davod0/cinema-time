7af3de8a89c588ff767f980086cda0f5c5da21c5
Davo <davodnikoyi.gmail.com>
2024-04-04 00:15:07 +0200

    The ReservationDetails Razor page is complete for displaying a reservation, and the Cinema.Blazor project is also finished

--- Cinema.Blazor/Program.cs
+++ Cinema.Blazor/Program.cs
@@ -32,12 +32,3 @@ internal class Program
     }
 }
 
-/*
-      // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error", createScopeForErrors: true);
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-*/
