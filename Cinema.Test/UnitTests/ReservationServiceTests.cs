401868247b55a70048870a6b5dbca331c0da0f12
Davo <davodnikoyi.gmail.com>
2024-04-04 18:12:26 +0200

    The unit tests for ReservationService class is done successgully 1

---
+++ Cinema.Test/UnitTests/ReservationServiceTests.cs
@@ -0,0 +1,34 @@
+namespace Cinema.Test;
+using Cinema.Core;
+using System.Threading.Tasks;
+using Moq;
+using System.Collections.Generic;
+
+public class ReservationServiceTests
+{
+    [Fact]
+    public async Task AddReservationAsyncc_ShouldAddReservationAndReturnAddedReservation()
+    {
+        // Arrange
+        Reservation res = new Reservation() { Id = 1, CinemaViewingId = 2, Quantity = 2 };
+        var mockReservationRepo = new Mock<IReservationRepository>();
+        mockReservationRepo.Setup(repo => repo.AddReservationAsync(res)).ReturnsAsync(res);
+
+        List<CinemaViewing> listOfCv = new() { new CinemaViewing { Id = 2, PlaceQuantity = 10 } };
+        var mockCinemaViewingRepo = new Mock<ICinemaViewingRepository>();
+        mockCinemaViewingRepo.Setup(repo => repo.GetAllCinemaViewingsAsync()).ReturnsAsync(listOfCv);
+
+
+        // Act
+        ReservationService resService = new(mockReservationRepo.Object, mockCinemaViewingRepo.Object);
+
+        var addedReservation = resService.AddReservationAsync(res);
+
+        // Assert
+        Assert.NotNull(addedReservation);
+        Assert.Equal(res.Id, addedReservation.Id);
+        Assert.Equal(res.CinemaViewingId, addedReservation.Result.CinemaViewingId);
+        Assert.Equal(8, addedReservation.Result.CinemaViewing.PlaceQuantity);
+    }
+}
+
