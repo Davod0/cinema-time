9ff1e73dc4f48ff6cff8fbee0b087cd6538feaba
Davo <davodnikoyi.gmail.com>
2024-04-04 20:01:04 +0200

    The unit tests for CinemaViewingService is done successfully

--- Cinema.Test/UnitTests/CinemaViewingServiceTests.cs
+++ Cinema.Test/UnitTests/CinemaViewingServiceTests.cs
@@ -7,15 +7,55 @@ using System.Collections.Generic;
 public class CinemaViewingServiceTests
 {
     [Fact]
+    public async Task AddCinemaViewingAsync_ShouldNotAddCinemaViewingDueToConflictAtSameTimeSameSalon()
+    {
+        // Arrange
+        CinemaViewing cv = new() { Id = 1, SalonId = 1, TimeAndDate = new DateTime(2024, 4, 4, 20, 30, 00), MovieId = 5 };
+        var mockCinemaViewingRepo = new Mock<ICinemaViewingRepository>();
+        List<CinemaViewing> listOfCv = new() { new CinemaViewing { Id = 2, SalonId = 1, TimeAndDate = new DateTime(2024, 4, 4, 20, 30, 00), MovieId = 10 } };
+
+        mockCinemaViewingRepo.Setup(repo => repo.GetAllCinemaViewingsAsync()).ReturnsAsync(listOfCv);
+        mockCinemaViewingRepo.Setup(repo => repo.AddCinemaViewingAsync(cv)).ReturnsAsync(cv);
+
+        Movie m = new() { Id = 5 };
+        var mockMovieRepo = new Mock<IMovieRepository>();
+        mockMovieRepo.Setup(repo => repo.GetMovieByIdAsync(m.Id)).ReturnsAsync(m);
+
+        // Act
+        CinemaViewingService cvs = new(mockCinemaViewingRepo.Object, mockMovieRepo.Object);
+
+        CinemaViewing addedCv = await cvs.AddCinemaViewingAsync(cv);
+
+        // Assert
+        Assert.Null(addedCv);
+    }
+
+    [Fact]
     public async Task AddCinemaViewingAsync_ShouldAddCinemaViewingAndReturnAddedCinemaViewing()
     {
         // Arrange
+        CinemaViewing cv = new() { Id = 1, SalonId = 1, TimeAndDate = new DateTime(2024, 05, 04, 20, 30, 00), MovieId = 5, };
+        var mockCinemaViewingRepo = new Mock<ICinemaViewingRepository>();
+        List<CinemaViewing> listOfCv = new() { new CinemaViewing { Id = 2, SalonId = 1, TimeAndDate = new DateTime(2024, 05, 05, 20, 30, 00), MovieId = 10 } };
 
+        mockCinemaViewingRepo.Setup(repo => repo.GetAllCinemaViewingsAsync()).ReturnsAsync(listOfCv);
+        mockCinemaViewingRepo.Setup(repo => repo.AddCinemaViewingAsync(cv)).ReturnsAsync(cv);
+
+        Movie m = new() { Id = 5 };
+        var mockMovieRepo = new Mock<IMovieRepository>();
+        mockMovieRepo.Setup(repo => repo.GetMovieByIdAsync(m.Id)).ReturnsAsync(m);
 
         // Act
+        CinemaViewingService cvs = new(mockCinemaViewingRepo.Object, mockMovieRepo.Object);
 
+        CinemaViewing addedCv = await cvs.AddCinemaViewingAsync(cv);
 
         // Assert
-
+        Assert.NotNull(addedCv);
+        Assert.Equal(cv.Id, addedCv.Id);
+        Assert.Equal(cv.SalonId, addedCv.SalonId);
+        Assert.Equal(cv.TimeAndDate, addedCv.TimeAndDate);
+        Assert.Equal(cv.MovieId, addedCv.MovieId);
     }
 }
+
